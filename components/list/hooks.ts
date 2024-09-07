951f994fb7385edeb36d257713131e0345ad8fda
zhanghaibin <zhang_h_bin@sina.com>
2024-09-07 21:25:00 +0800

    feat: 0.0.12

---
+++ components/list/hooks.ts
@@ -0,0 +1,9 @@
+import { ref } from 'vue';
+export const useList = () => {
+  const listRef: any = ref(null);
+  return {
+    listRef,
+    refresh: () => listRef.value?.refresh(),
+    nextPage: () => listRef.value?.nextPage()
+  };
+};
